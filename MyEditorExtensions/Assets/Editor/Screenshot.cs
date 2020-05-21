9f1da43074a616b04113c67ae5a155dae524d58b
romaneL8 <65686183+romaneL8@users.noreply.github.com>
2020-05-21 16:53:57 -0400

    Screenshot Creator Extension

---
+++ MyEditorExtensions/Assets/Editor/Screenshot.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[CustomEditor(typeof(ScreenshotHandeler))]
+public class Screenshot : Editor
+{
+    string fileName = "FileName";
+    int selectedAction = 0;
+    //int capturedFrame = 0;
+    private Camera myCamera;
+
+    //[MenuItem("Screenshot/Screenshot")]
+
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(Screenshot));
+    }
+
+    public override void OnInspectorGUI()
+    {
+        fileName = EditorGUILayout.TextField("File Name:", fileName);
+        GUILayout.Label("Screenshot Settings", EditorStyles.boldLabel);
+        myCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        float width = 200;
+        float height = 30;
+        string[] actionLabels = new string[] { "Take", "Delete", "Edit" };
+        selectedAction = GUILayout.SelectionGrid(selectedAction, actionLabels, 3, GUILayout.Width(width), GUILayout.Height(height));
+        float heightCam = 16;
+        float weightCam = 16;
+        myCamera.pixelRect = new Rect(10, 0, weightCam, heightCam);
+        Handles.SetCamera(myCamera);
+        Handles.DrawCamera(myCamera.pixelRect, myCamera, DrawCameraMode.Normal);
+        //Handles.DrawCamera(new Rect(0, 0, position.width, position.height), myCamera);
+        Handles.BeginGUI();
+        //myCamera.hideFlags = HideFlags.HideAndDontSave;
+        //var previewScene = EditorSceneManager.NewPreviewScene();
+        //myCamera.scene = previewScene;
+        //myCamera.enabled = false;
+        if (selectedAction == 0)
+        {
+            ScreenshotHandeler.TakeScreenshot_Static(500, 500);
+            //ScreenCapture.CaptureScreenshot(fileName + " " + capturedFrame + ".png");
+        }
+    }
+
+    //void OnScene(SceneView sceneview)
+    //{
+    //    Event e = Event.current;
+    //    if (e.type == EventType.MouseUp)
+    //    {
+    //        Ray r = Camera.current.ScreenPointToRay(new Vector3(e.mousePosition.x, Camera.current.pixelHeight - e.mousePosition.y));
+    //        if (selectedAction == 1)
+    //        {
+    //            //TakeScreenshot_Static(500, 500);
+    //            // TODO: Create OpenSpot
+    //            Debug.Log("Create OpenSpot");
+    //        }
+    //        else if (selectedAction == 2)
+    //        {
+    //            // TODO: Delete OpenSpot
+    //            Debug.Log("Delete OpenSpot");
+    //        }
+    //    }
+    //}
+
+    //void OnEnable()
+    //{
+    //    SceneView.duringSceneGui -= OnScene;
+    //    SceneView.duringSceneGui += OnScene;
+    //}
+
+    //private void Update()
+    //{
+    //    if (EditorApplication.isPlaying && !EditorApplication.isPaused)
+    //    {
+    //        if (selectedAction == 2)
+    //        {
+    //            ScreenCapture.CaptureScreenshot(fileName + " " + capturedFrame + ".png");
+    //            Repaint();
+    //        }
+    //    }
+    //}
+}
---
+++ MyEditorExtensions/Assets/ScreenshotHandeler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotHandeler : MonoBehaviour
+{
+    private static ScreenshotHandeler _instance;
+    private bool takeScreenshotOnNextFrame;
+    private Camera myCamera;
+    private int count;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        //instance = this;
+        myCamera = gameObject.GetComponent<Camera>();
+    }
+
+    private void OnPostRender()
+    {
+        if (takeScreenshotOnNextFrame)
+        {
+            takeScreenshotOnNextFrame = false;
+            RenderTexture renderTexture = myCamera.targetTexture;
+
+            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
+            renderResult.ReadPixels(rect, 0, 0);
+            renderResult.Apply();
+
+            byte[] byteArray = renderResult.EncodeToPNG();
+            System.IO.File.WriteAllBytes(Application.dataPath + "/CameraScreenshot.png" + count, byteArray);
+            Debug.Log("Saved CameraScreenshot.png");
+
+            RenderTexture.ReleaseTemporary(renderTexture);
+            myCamera.targetTexture = null;
+        }
+    }
+
+    private void TakeScreenshot(int width, int height)
+    {
+        myCamera.targetTexture = RenderTexture.GetTemporary(width, height, 16);
+        takeScreenshotOnNextFrame = true;
+    }
+
+    public static void TakeScreenshot_Static(int width, int height)
+    {
+        _instance.TakeScreenshot(width, height);
+    }
+}
